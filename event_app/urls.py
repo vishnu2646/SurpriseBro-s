2fbc0cdcfa933f95f32487090b353dcdd1a5bc6b
vishnupriyan <priyanvishnu15@gmail.com>
2020-12-04 15:56:50 +0530

    update

--- event_app/urls.py
+++ event_app/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
-from . import views
+from . import views 
+from .views import (
+    EventListView,
+    EventDetailView
+)
 
 urlpatterns = [
     path('',views.index,name="home"),
+    path('test/',views.test,name="test"),
     path('gallery/',views.gallery,name="gallery"),
     path('about/',views.about,name="about"),
-    path('contact/',views.contact,name="contact"),
-    path('details/',views.details,name="details"),
-    path('event/',views.event,name="event"),
+    path('contact/',views.ContactView.as_view(),name="contact"),
+    path('details/<int:pk>/',views.EventDetailView.as_view(),name="event-details"),
+    path('event/',views.EventListView.as_view(),name="event"),
     path('booking/',views.booking,name="booking"),
     path('404/',views.error,name="404-error"),
 ]
--- event_app/views.py
+++ event_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import *
+from django.views.generic import View,ListView,DetailView
 # Create your views here.
 def index(request):
     return render(request,'index.html')
@@ -13,19 +14,40 @@ def about(request):
 def contact(request):
     return render(request,'contact.html')
 
-def details(request):
-    return render(request,'details.html')
+class EventListView(ListView):
+    model = Event
+    template_name = 'event.html'
+    context_object_name = 'events'
+    paginate_by = 4
+    queryset = Event.objects.order_by('-event_name')[:6]
 
-def event(request):
-    return render(request,'event.html')
+class EventDetailView(DetailView):
+    model = Event
+    template_name = 'details.html'
 
 def error(request):
     return render(request,'404-error.html')
 
 def booking(request):
     return render(request,'booking.html')
-'''
+
+def test(request):
+    return render(request,'test.html')
+
 class ContactView(View):
-    template_name='contact.html'
-    def get(self,requst):
-        contacts = Contact.objects.all()'''
+    template_name = 'contact.html'
+    raise_exception = True
+    permission_denied_message = 'You must Login Now.'
+    def get(self,request):
+        return render(request,self.template_name)
+    def post(self,request):
+        data = request.POST
+        contact = Contact()
+        contact.name = data['name']
+        contact.mail =data['mail']
+        contact.counrty = data['counrty']
+        contact.phone = data['phone']
+        contact.message = data['message']
+        contact.save()
+        return redirect("contact")
+        return render(request,self.template_name)
